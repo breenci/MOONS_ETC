6ff401b272234f0d1dfa08a99400142e8e35ad87
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-03-04 15:45:57 +0000

    Add form to input values to etc backend configuration file
    Create form using flask-bootstrap and WTForms. Add form.html
    template to render the form extending from flask-bootstrap
    templates. Create wtform object in forms.py. Add form to view
    function and set configuration file variable is form is
    validated.

---
+++ app/forms.py
@@ -0,0 +1,34 @@
+from os import system
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, IntegerField, SubmitField, FloatField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    # target variables
+    template_name = StringField('Template', validators=[DataRequired()])
+    magnitude = FloatField('Magnitude', validators=[DataRequired()])
+    #filter = SelectField('Filter', choices=['H', 'YJ'], validators=[DataRequired()])
+    #system = SelectField('System', choices=['AB', 'BA'])
+    #reddening = FloatField('Reddening')
+    #displacement = FloatField('Displacement')
+
+    # instrument variables
+    mode = SelectField('Mode', choices=['HR', 'LR'],validators=[DataRequired()])
+    strategy = SelectField('Strategy', choices=['Xswitch', 'STARE'],validators=[DataRequired()])
+    # adc_refwave = FloatField('ADC_refwave')
+    # exp_time = FloatField('Exposure Time')
+    # nexp = IntegerField('No. Exposures')
+    # dit = IntegerField('dit')
+
+    #conditions
+    seeing = FloatField('Seeing', validators=[DataRequired()])
+    airmass = FloatField('Airmass',validators=[DataRequired()])
+
+    #simulation
+    sky_temp = StringField('Sky Template',validators=[DataRequired()])
+    sky_res = IntegerField('Sky Residual',validators=[DataRequired()])
+    # tell = IntegerField('Telluric')
+    # flux_cal = FloatField('Flux Calib')
+
+    #control
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
 from app import app
 from flask import render_template
+import configparser
 
-@app.route('/')
-@app.route('/index')
+from app.forms import LoginForm
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        config = configparser.ConfigParser()
+        config.read('ParamFile.ini')
+        config.set('target', 'template_name', form.template_name.data)
+        config.set('target', 'magnitude', str(form.magnitude.data))
+        config.set('instrument', 'moons_mode', form.mode.data)
+        config.set('instrument', 'strategy', form.strategy.data)
+        config.set('conditions', 'seeing', str(form.seeing.data))
+        config.set('conditions', 'airmass', str(form.airmass.data))
+        config.set('simulation', 'sky_template', form.sky_temp.data)
+        config.set('simulation', 'sky_residual', str(form.sky_res.data))
+
+        with open('ParamFile.ini', 'w') as configfile:
+            config.write(configfile)
+
+    return render_template('form.html', form=form)
