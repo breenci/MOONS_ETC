75d43893c087f955c3031181f61a75f1222197bb
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-03-07 11:12:15 +0000

    Add the rest of the form fields

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,32 @@ from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
+    form = LoginForm(template_name = 'test1')
     if form.validate_on_submit():
         config = configparser.ConfigParser()
         config.read('ParamFile.ini')
+
         config.set('target', 'template_name', form.template_name.data)
         config.set('target', 'magnitude', str(form.magnitude.data))
+        config.set('target', 'filter', form.filter.data)
+        config.set('target', 'system', form.system.data)
+        config.set('target', 'reddening', str(form.reddening.data))
+        config.set('target', 'displacement', str(form.displacement.data))
+
         config.set('instrument', 'moons_mode', form.mode.data)
         config.set('instrument', 'strategy', form.strategy.data)
+        config.set('instrument', 'adc_refwave', str(form.adc_refwave.data))
+        config.set('instrument', 'exp_time', str(form.exp_time.data))
+        config.set('instrument', 'nexp', str(form.nexp.data))
+        config.set('instrument', 'dit', str(form.dit.data))
+
         config.set('conditions', 'seeing', str(form.seeing.data))
         config.set('conditions', 'airmass', str(form.airmass.data))
+
         config.set('simulation', 'sky_template', form.sky_temp.data)
         config.set('simulation', 'sky_residual', str(form.sky_res.data))
+        config.set('simulation', 'telluric', str(form.tell.data))
+        config.set('simulation', 'flux_calib', str(form.flux_cal.data))
 
         with open('ParamFile.ini', 'w') as configfile:
             config.write(configfile)
