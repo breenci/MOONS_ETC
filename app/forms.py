596a67bad65025dc6bed0fa64a7e28bded324d6e
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-03-07 11:14:40 +0000

    Uncomment rest of the form fields in forms.py

--- app/forms.py
+++ app/forms.py
@@ -7,18 +7,18 @@ class LoginForm(FlaskForm):
     # target variables
     template_name = StringField('Template', validators=[DataRequired()])
     magnitude = FloatField('Magnitude', validators=[DataRequired()])
-    #filter = SelectField('Filter', choices=['H', 'YJ'], validators=[DataRequired()])
-    #system = SelectField('System', choices=['AB', 'BA'])
-    #reddening = FloatField('Reddening')
-    #displacement = FloatField('Displacement')
+    filter = SelectField('Filter', choices=['H', 'YJ'], validators=[DataRequired()])
+    system = SelectField('System', choices=['AB', 'BA'])
+    reddening = FloatField('Reddening')
+    displacement = FloatField('Displacement')
 
     # instrument variables
     mode = SelectField('Mode', choices=['HR', 'LR'],validators=[DataRequired()])
-    strategy = SelectField('Strategy', choices=['Xswitch', 'STARE'],validators=[DataRequired()])
-    # adc_refwave = FloatField('ADC_refwave')
-    # exp_time = FloatField('Exposure Time')
-    # nexp = IntegerField('No. Exposures')
-    # dit = IntegerField('dit')
+    strategy = SelectField('Strategy', choices=['Xswitch', 'STARE'], validators=[DataRequired()])
+    adc_refwave = FloatField('ADC_refwave')
+    exp_time = FloatField('Exposure Time')
+    nexp = IntegerField('No. Exposures')
+    dit = IntegerField('dit')
 
     #conditions
     seeing = FloatField('Seeing', validators=[DataRequired()])
@@ -27,8 +27,8 @@ class LoginForm(FlaskForm):
     #simulation
     sky_temp = StringField('Sky Template',validators=[DataRequired()])
     sky_res = IntegerField('Sky Residual',validators=[DataRequired()])
-    # tell = IntegerField('Telluric')
-    # flux_cal = FloatField('Flux Calib')
+    tell = IntegerField('Telluric')
+    flux_cal = FloatField('Flux Calib')
 
     #control
     submit = SubmitField('Submit')
