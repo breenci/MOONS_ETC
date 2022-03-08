0e38824e2e68dec573ea585a8a527c8b883c3b2d
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-03-08 14:40:11 +0000

    Change some form field types to match data types

--- app/forms.py
+++ app/forms.py
@@ -5,18 +5,18 @@ from wtforms.validators import DataRequired
 
 class ETC_form(FlaskForm):
     # target variables
-    template_name = StringField('Template', validators=[DataRequired()])
+    template_name = SelectField('Template', choices=[('constant_in_wav', 'Example_spectra/constant_in_wav.fits'), ('stellar', 'Example_spectra/input_stellar_template.fits')], validators=[DataRequired()])
     magnitude = FloatField('Magnitude', validators=[DataRequired()])
-    filter = SelectField('Filter', choices=['H', 'YJ'], validators=[DataRequired()])
-    system = SelectField('System', choices=['AB', 'BA'])
+    filter = SelectField('Filter', choices=['H', 'J', 'I'], validators=[DataRequired()])
+    system = SelectField('System', choices=['AB', 'Vega'])
     reddening = FloatField('Reddening')
     displacement = FloatField('Displacement')
 
     # instrument variables
     mode = SelectField('Mode', choices=['HR', 'LR'],validators=[DataRequired()])
-    strategy = SelectField('Strategy', choices=['Xswitch', 'STARE'], validators=[DataRequired()])
+    strategy = SelectField('Strategy', choices=['Xswitch', 'Stare'], validators=[DataRequired()])
     adc_refwave = FloatField('ADC_refwave')
-    exp_time = FloatField('Exposure Time')
+    exp_time = IntegerField('Exposure Time')
     nexp = IntegerField('No. Exposures')
     dit = IntegerField('dit')
 
@@ -25,7 +25,7 @@ class ETC_form(FlaskForm):
     airmass = FloatField('Airmass',validators=[DataRequired()])
 
     #simulation
-    sky_temp = StringField('Sky Template',validators=[DataRequired()])
+    sky_temp = SelectField('Sky Template', choices=['eso_skycalc'], validators=[DataRequired()])
     sky_res = IntegerField('Sky Residual',validators=[DataRequired()])
     tell = IntegerField('Telluric')
     flux_cal = FloatField('Flux Calib')
