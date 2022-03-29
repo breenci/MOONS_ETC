c77eb0d65160c44b13736487b85c326546d0a4cc
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-03-29 16:53:13 +0100

    Add session functionality to solve multiple user issues
    Use Flask Session package to track sessions. This will be used
    to store the output files for each user in a unique location.

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SESSION_PERMANENT = False
+    SESSION_TYPE = 'filesystem'
+    SESSION_FILE_THRESHOLD = 2
