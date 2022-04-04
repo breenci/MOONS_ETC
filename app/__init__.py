cf3487ccd8c27ea15699856b323ad3dfe04460e5
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2022-04-04 09:29:18 +0100

    Remove flask session functionality
    No longer using this approach to store user data. See previous
    commit.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,9 @@ from ensurepip import bootstrap
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
-from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
-Session(app)
 
 from app import routes
--- config.py
+++ config.py
@@ -2,6 +2,3 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SESSION_PERMANENT = False
-    SESSION_TYPE = 'filesystem'
-    SESSION_FILE_THRESHOLD = 2
